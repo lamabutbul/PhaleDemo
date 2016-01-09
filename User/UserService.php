290c63637d241c72f4266695c5cffca102c681c8
lamabutbul <lamabutbul@gmail.com>
2016-01-09 19:32:03 +0200

    Create UserService.php

---
+++ User/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+class UserService implements IUserService {
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function find($id) {
+        // TODO: Implement find() method.
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findAll() {
+        // TODO: Implement findAll() method.
+    }
+
+}
